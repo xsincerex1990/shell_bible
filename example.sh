b728daa3ff3fb344c74f7ef59e8ebeba7d64ca9c
fruitdealer <xsincerex1990@gmail.com>
2018-10-30 17:57:36 -0400

    linux is the shit

--- example.sh
+++ example.sh
@@ -5,7 +5,8 @@ set -e
 ARG1="$1"
 ARG2="$2"
 ARG3="$3"
+SOMETHING="WE DA SHIT BITCHHHH, THE NEW MTF RELIGION";
+echo "${ARG1} is my name, ${ARG2} is yours, ${ARG3} our boy.";
 
-echo "${ARG} is my name, ${ARG2} is yours, ${ARG3} our boy."
-
-echo ${SOMETHING}
+for i in {1..10}; do echo "${SOMETHING} $i"; 
+done
